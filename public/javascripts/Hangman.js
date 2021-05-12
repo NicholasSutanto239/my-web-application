98c7d59a07afb47c21bde17523bc3a90d8b53ac5
NicholasSutanto239 <sutanton@spu.edu>
2021-05-11 20:58:17 -0700

    UPDATES

---
+++ public/javascripts/Hangman.js
@@ -0,0 +1,5 @@
+var Car_brands = ["TOYOTA", "HONDA", "NISSAN", "FORD", "CHEVROLET", "MERCEDES", "BMW",
+                    "TESLA", "JEEP", "SUBARU"];
+
+var Sports = ["BASKETBALL", "SOCCER", "FOOTBALL", "TENNIS", "BADMINTON", "SWIMMING", "BASEBALL",
+                    "CRICKET", "HOCKEY", "WRESTLING"];
